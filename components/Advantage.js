22725367fb7261f496c1daff90dfd0258e6d397e
Mansour Ahmed <73757452+theEngMansour@users.noreply.github.com>
2022-05-01 02:01:01 +0300

    Update Advantage.js

--- components/Advantage.js
+++ components/Advantage.js
@@ -4,12 +4,12 @@ export default function Advantage() {
   return (
     <div className="bg-gray-100">
         <section className="mx-auto max-w-7xl sm:px-6 lg:px-8 bg-white pt-16">
-            <div role="contentinfo" className="flex items-center flex-col px-4">
+            <div className="flex items-center flex-col px-4">
                 <h1  className="focus:outline-none text-4xl lg:text-4xl font-extrabold text-center leading-10 text-[#00c37d] lg:w-5/12 md:w-9/12 pt-4">مميزات الموقع</h1>
                 <p  className="focus:outline-none text-sm text-center mt-3 text-[#00c37d] leading-4 font-mono">mansour_tech@</p>
             </div>
-            <div  aria-label="group of cards" className="focus:outline-none mt-20 flex flex-wrap justify-start gap-10 px-4">
-                <div  aria-label="card 1" className="focus:outline-none flex sm:w-full md:w-5/12 pb-20">
+            <div className="focus:outline-none mt-20 flex flex-wrap justify-start gap-10 px-4">
+                <div className="focus:outline-none flex sm:w-full md:w-5/12 pb-20">
                     <div className="w-20 h-20 relative mr-5">
                         <div className="absolute top-0 right-0 bg-[#00c37c9d] rounded w-16 h-16 mt-2 mr-1"></div>
                         <div className="absolute text-white bottom-0 left-0 bg-[#00c37d] rounded w-16 h-16 flex items-center justify-center mt-2 mr-3">
@@ -21,7 +21,7 @@ export default function Advantage() {
                         <p  className="focus:outline-none text-base text-gray-600 leading-normal pt-2">موقع يقدم خدمة سريعة</p>
                     </div>
                 </div>
-                <div  aria-label="card 1" className="focus:outline-none flex sm:w-full md:w-5/12 pb-20">
+                <div className="focus:outline-none flex sm:w-full md:w-5/12 pb-20">
                     <div className="w-20 h-20 relative mr-5">
                         <div className="absolute top-0 right-0 bg-[#00c37c9d] rounded w-16 h-16 mt-2 mr-1"></div>
                         <div className="absolute text-white bottom-0 left-0 bg-[#00c37d] rounded w-16 h-16 flex items-center justify-center mt-2 mr-3">
@@ -45,7 +45,7 @@ export default function Advantage() {
                         <p  className="focus:outline-none text-base text-gray-600 leading-normal pt-2">جميع الصور الذي في الموقع ذا دقة عالية</p>
                     </div>
                 </div>
-                <div  aria-label="card 4" className="focus:outline-none flex sm:w-full md:w-5/12 pb-20">
+                <div className="focus:outline-none flex sm:w-full md:w-5/12 pb-20">
                     <div className="w-20 h-20 relative mr-5">
                         <div className="absolute top-0 right-0 bg-[#00c37c9d] rounded w-16 h-16 mt-2 mr-1"></div>
                         <div className="absolute text-white bottom-0 left-0 bg-[#00c37d] rounded w-16 h-16 flex items-center justify-center mt-2 mr-3">
